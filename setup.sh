7bf0109c98c04787dad566a8c22d65f8bf982f3a
tsalvia <46477802+tsalvia@users.noreply.github.com>
2021-09-22 00:10:08 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ mkdir -p $HOME/.vim/bundle
 cd $HOME/.vim/bundle
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh $HOME/.vim/bundle
-ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+ln -fs $HOME/.vim/.vimrc $HOME/.vimrc
 vim -c ":silent! call dein#install() | :q"
